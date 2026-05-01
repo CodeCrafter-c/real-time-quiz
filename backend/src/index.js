50cedee2fc623880b101b18c9b0f1d9f35a9a5e6
CodeCrafter-c <anshsharma141341@gmail.com>
2026-05-01 23:17:35 +0530

    socket added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,46 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 import connectToDb from "./db/connection.js";
 import userRouter from "./routes/user.js";
-import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true
+  }
+});
 app.use(cookieParser());
+app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+app.use("/api/v1/users", userRouter);
+
+io.on("connection", (socket) => {
+  console.log("user connected:", socket.id);
+});
 
 const port = 3000;
 
 async function startServer() {
   await connectToDb();
 
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`server running on port ${port}`);
   });
 }
 
-
-app.use("/api/v1/users",userRouter);
 startServer();
